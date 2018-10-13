73e0ea81d29679bc01a40194b50dd9dce36716d3
little-brother <lb.im@ya.ru>
2018-10-13 21:40:48 +0300

    i

---
+++ index.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function() {
+	console.time('start');
+	var dict = localStorage.getItem('word-ladder-dictionary');
+	console.log('DICT', (dict||'').length);
+	if (!dict) {
+	console.timeEnd('start');
+
+		console.time('load');
+		fetch('dictionary.json')
+			.then(function (response){
+			 	response => response.json() 	
+				
+			})
+			.then(function (json) {
+				dict = json;
+				console.timeEnd('load');
+				localStorage.setItem('word-ladder-dictionary', JSON.stringify(response.json))
+			});
+	} else  {
+		dict = JSON.parse(dict);
+		console.timeEnd('start')
+	}
+});
